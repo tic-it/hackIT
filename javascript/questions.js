aeae9dd41a3484f9f6cb76992ae948135a588c77
gurung112 <140948353+gurung112@users.noreply.github.com>
2023-10-05 10:14:07 +0545

    Add files via upload

---
+++ javascript/questions.js
@@ -0,0 +1,23 @@
+// let dolphinA=96
+// let dolphinB=108
+// let dolphinC=89
+// let koalaA=88
+// let koalaB=91
+// let koalaC=110
+// let averageDolphin=(dolphinA+dolphinB+dolphinC)/3
+// let averageKoala=(koalaA+koalaB+koalaC)/3
+// console.log(averageDolphin>averageKoala ? ` the winner of the competition is Dolphin` : ` the winner of the competition is Koala`)
+// console.log(averageDolphin>averageKoala ? ` the winner of the competition is Dolphin` : averageDolphin<averageKoala ? ` the winner of the competition is Koala` : `its a draw`)
+
+// let dolphinA=97
+// let dolphinB=112
+// let dolphinC=101
+// let koalaA=109
+// let koalaB=95
+// let koalaC=123
+
+
+
+
+
+
