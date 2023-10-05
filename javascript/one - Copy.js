9dc926d57d8096b17e6cca9ab754d5b7857b3f02
anushkrit <147010601+anushkrit@users.noreply.github.com>
2023-10-04 21:11:37 -0700

    added a javascript file

---
+++ javascript/one - Copy.js
@@ -0,0 +1,8 @@
+//wap to print 1 is odd beacuse it is not divisable by 2
+for (let i = 1; i <= 20; i++) {
+    if (i%2 !==0)
+    {
+        console.log("${i} it is odd beacause ti is divisable by 2")
+    }
+
+    }
