fd5d8ef4c12f5db55738066182f52229db3eed3f
Rishirk98 <137885628+Rishirk98@users.noreply.github.com>
2023-10-05 10:13:13 +0545

    Add files via upload

---
+++ C/operators.js
@@ -0,0 +1,67 @@
+// presidense   and  assositivity
+
+
+
+
+// Types of operators
+// a.arthimetic operators
+    //i.addition operators(+)
+    //ii.subtraction operators(-)
+    //iii.multiplication operators(*)
+    //iv.division operators(/)
+    //v.modulus operators(%)
+    //vi.exponential operators(**)
+    //ii.concat operators (+)     incase of strings in js (+) sign acts as concat operator.
+                // eg:"ram"+"sita"               true+"hello"
+                // output:"ramsita"             output: "truehello"
+
+// example
+// let a=1
+// let b=2
+// let sum=a+b
+// console.log(sum)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(a**2)
+
+
+// b. assignment operators
+    // i. assignment operator(=)
+    //ii. addition operator(+=)
+    //iii.subtraction operator(-=)
+    //iv.muliiple operator(*=)
+     //v.division operator (/=)
+    // vi.modulus operator(%=)
+    //vii. exponential operator(**=)
+
+
+    // let x=5
+    // x += 3        (x=x+3)
+    // console.log(x)                 and so on.
+
+    
+
+// c. comparison(relational) operators
+// d. logical operators
+// e. special operators
+ 
+// VVIMP
+// type coercion:-it is the default behaviour of JS that forcefully changes one datatype to 
+//                 another datatype to execute the operations successfully.
+//                  null/false=0   true=1
+// type conversion:-converison of one data type to another datatype according to  developers will to generate output. 
+//   parseInt('20.5')=20
+//   parseFloat("20.5")=20.5
+//   number('20')=20  
+//   string(20)='20'
+//   boolean(undefined)=false
+                //Truthy and falsy values in JS   (falsy =empty ())
+//0,0n-0,"",false,undefnied ,nul,NaN are falsy values in javascript.(0n= Bigint ko value)
+//Except these values all the values are considered as truthy values during the condition/Boolean check.
+
+
+
+
+
