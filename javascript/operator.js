f361020586862cb770642fb40a3ce10dc114499c
snehamrj <147018910+snehamrj@users.noreply.github.com>
2023-10-05 10:05:09 +0545

    added javascript file

---
+++ javascript/operator.js
@@ -0,0 +1,2 @@
+// Operators are signs & symbols that are used to make expressions out of operands and manipulate the value.
+// Type conversion is the method to convert one datatype to another datatype by the user
