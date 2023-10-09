b5a9ae24e1486c149ce4f272af68a52fbe48e6bd
Aaradhya Pathak <aaradhyapathakofficial@gmail.com>
2023-10-09 10:45:16 +0530

    findDolphin function using Switch

--- javascript/challenge1.js
+++ javascript/challenge1.js
@@ -97,3 +97,25 @@
 // arr.forEach((animal) => {
 //   console.log("My " + animal);
 // });
+
+// 6
+// const findDolphin = (hint) => {
+//   switch (hint) {
+//     case "fish":
+//       console.log("Yes it is a fish");
+//       break;
+
+//     case "blue":
+//       console.log("Yes its blue");
+//       break;
+
+//     case "bigBrain":
+//       console.log("Yes They have big brain");
+//       break;
+
+//     default:
+//       break;
+//   }
+// };
+
+// findDolphin("bigBrain");
